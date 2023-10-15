add6d3eaefefbb25236e773b2b9d5a44868b2958
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-10-15 18:41:41 +0300

    Added useful comments

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,9 +4,9 @@ import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashedPassword });
+  const { username, email, password } = req.body; //First we destructure the username, email & password
+  const hashedPassword = bcryptjs.hashSync(password, 10); //Then we hash the password
+  const newUser = new User({ username, email, password: hashedPassword }); //We instantiate a new User and add the data for the username, email...
   try {
     res.setHeader('Content-Type', 'application/json');
     await newUser.save();
@@ -19,12 +19,12 @@ export const signup = async (req, res, next) => {
 };
 
 export const signin = async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body; //First we destructure the email & password
   try {
-    const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, 'User not found!'));
-    const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+    const validUser = await User.findOne({ email }); //Then we test if the email exists
+    if (!validUser) return next(errorHandler(404, 'User not found!')); // if it does not exist we  give the user the response 404
+    const validPassword = bcryptjs.compareSync(password, validUser.password); // We also check the password entered against the hashed one in the db
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!')); // If it does not exist we return a 401 response
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
     res
@@ -32,6 +32,6 @@ export const signin = async (req, res, next) => {
       .status(200)
       .json(rest);
   } catch (error) {
-    next(error);
+    next(error); //Using Middleware and our tey and catch statement we give the user the appropriate error message
   }
 };
