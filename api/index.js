17c5a1a09a7c24e04ea0254d30effae4ad4d5140
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-11-27 09:38:25 +0300

    Feat: added api route for creating a listing

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import listingRouter from './routes/listing.routes.js';
 
 dotenv.config();
 
@@ -32,6 +33,8 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
