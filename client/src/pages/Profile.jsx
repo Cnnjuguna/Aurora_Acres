06bd15e57c946bd881e47390fa1bc38b2f084118
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-11-28 11:17:23 +0300

    Feat: Completed uploading listing images functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -19,6 +19,7 @@ import {
   updateUserStart,
   updateUserSuccess,
 } from '../redux/user/userSlice';
+import { Link } from 'react-router-dom';
 
 function Profile() {
   const dispatch = useDispatch();
@@ -181,6 +182,12 @@ function Profile() {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to={'/create-listing'}
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
