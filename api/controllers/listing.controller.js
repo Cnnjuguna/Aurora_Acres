cf38994fe9cef8654d3b4b13f4c655d9dafda741
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-12-08 18:02:19 +0300

    Feat: Added search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -68,3 +68,53 @@ export const getListing = async (req, res, next) => {
         next(error);
     }
 };
+
+export const getListings = async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9; //If there is a limit parse it to a number, otherwise use 9 as the limit
+        res.status(200).json(listing);
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        let offer = req.query.offer;
+
+        if (offer === undefined || offer === false) {
+            offer = { $in: [false, true] }; // Search the listing which can be either true of false depending on wha they select
+        }
+
+        let furnished = req.query.furnished;
+
+        if (furnished === undefined || furnished === false) {
+            furnished = { $in: [false, true] }; // Search the listing  furnished which can be either true of false depending on what they select
+        }
+
+        let parking = req.query.parking;
+
+        if (parking === undefined || parking === false) {
+            parking = { $in: [false, true] };
+        }
+
+        let type = req.query.type;
+
+        if (type == undefined || type === 'all') {
+            type = { $in: ['sale', 'rent'] };
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+        const sort = req.query.sort || 'createdAt';
+        const order = req.query.order || 'desc';
+
+        const listings = await Listing.find({
+            name: { $regex: searchTerm, $options: 'i' },
+            offer,
+            furnished,
+            parking,
+            type,
+        })
+            .sort({ [sort]: order })
+            .limit(limit)
+            .skip(startIndex);
+
+        return res.status(200).json(listings);
+    } catch (error) {
+        next(error);
+    }
+};
