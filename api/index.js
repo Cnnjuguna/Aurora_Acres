63b75f9396324f1ddebeb437fd9dbbf05462b72d
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-12-13 12:05:03 +0300

    Enh: added pre-installation scripts

--- api/index.js
+++ api/index.js
@@ -6,18 +6,20 @@ import authRouter from './routes/auth.routes.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.routes.js';
+import path from 'path';
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-// const __dirname = path.resolve();
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -28,21 +30,27 @@ app.use(cookieParser());
 app.set('port', process.env.PORT || 3000);
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  return res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
 });
 
 app.use(cors());
